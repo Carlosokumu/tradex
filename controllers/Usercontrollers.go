26ad614462949b416c812797a3b9b069d5de1933
Carlos <carlosokumu254@gmail.com>
2022-07-19 12:55:01 +0300

    added a route

--- controllers/Usercontrollers.go
+++ controllers/Usercontrollers.go
@@ -28,7 +28,7 @@ func RegisterUser(context *gin.Context) {
 		return
 	}
 
-	//Create a new user record into the database
+	//Create a new user record into the databases
 
 	record := database.Instance.Create(&user)
 	if record.Error != nil {
