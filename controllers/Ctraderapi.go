098b80d7e7961442cd08a5c5c66588f839e6f846
Carlos <carlosokumu254@gmail.com>
2022-07-19 17:27:54 +0300

    added add position data

--- controllers/Ctraderapi.go
+++ controllers/Ctraderapi.go
@@ -1,13 +1,30 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/carlosokumu/dubbedapi/models"
 	"github.com/gin-gonic/gin"
 )
 
 func PositionData(context *gin.Context) {
+
+	bodyBytes, err := ioutil.ReadAll(context.Request.Body)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	var responseObject models.PositionData
+
+	parseError := json.Unmarshal(bodyBytes, &responseObject)
+
+	if parseError != nil {
+		fmt.Println(parseError)
+	}
+
 	fmt.Println("Ctrader Sendind data")
 	context.String(http.StatusOK, "Hellow")
 }
--- models/PositionData.go
+++ models/PositionData.go
@@ -1,12 +1,10 @@
 package models
 
 type PositionData struct {
-	AccountID                   int    `json:"accountId,omitempty"`
-	AccountNumber               int    `json:"accountNumber,omitempty"`
-	Live                        bool   `json:"live,omitempty"`
-	BrokerName                  string `json:"brokerName,omitempty"`
-	BrokerTitle                 string `json:"brokerTitle,omitempty"`
-	DepositCurrency             string `json:"depositCurrency,omitempty"`
-	TraderRegistrationTimestamp int64  `json:"traderRegistrationTimestamp,omitempty"`
-	TraderAccountType           string `json:"traderAccountType,omitempty"`
+	Data []struct {
+		PositionId int    `json:"positionid,omitempty"`
+		EntryPrice int    `json:"entryprice,omitempty"`
+		TradeType  bool   `json:"tradetype,omitempty"`
+		Quantity   string `json:quantity",omitempty"`
+	}
 }
