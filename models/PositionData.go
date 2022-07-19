70178eb2f7978fad197415ea3f762fa487005877
Carlos <carlosokumu254@gmail.com>
2022-07-19 16:19:44 +0300

    added position data

---
+++ models/PositionData.go
@@ -0,0 +1,13 @@
+
+
+
+type PositionData struct {
+	AccountID                   int    `json:"accountId,omitempty"`
+	AccountNumber               int    `json:"accountNumber,omitempty"`
+	Live                        bool   `json:"live,omitempty"`
+	BrokerName                  string `json:"brokerName,omitempty"`
+	BrokerTitle                 string `json:"brokerTitle,omitempty"`
+	DepositCurrency             string `json:"depositCurrency,omitempty"`
+	TraderRegistrationTimestamp int64  `json:"traderRegistrationTimestamp,omitempty"`
+	TraderAccountType           string `json:"traderAccountType,omitempty"`
+}
