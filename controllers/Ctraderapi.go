41810b475ec141b5cfece5f07830f794a75c1f91
Carlos <carlosokumu254@gmail.com>
2022-07-22 11:56:44 +0300

    added log Info

--- controllers/Ctraderapi.go
+++ controllers/Ctraderapi.go
@@ -25,6 +25,11 @@ func PositionData(context *gin.Context) {
 		fmt.Println(parseError)
 	}
 
-	fmt.Println("Ctrader Sendind data", responseObject.EntryPrice)
+	fmt.Println("Ctrader Sendind data:Entry Price ", responseObject.EntryPrice)
+	fmt.Println("Ctrader Sendind data:Position Id", responseObject.PositionId)
+	fmt.Println("Ctrader Sendind data:TradeType", responseObject.TradeType)
+	fmt.Println("Ctrader Sendind data:EntryTime", responseObject.EntryTime)
+	fmt.Println("Ctrader Sendind data:Quantity", responseObject.Quantity)
+
 	context.String(http.StatusOK, "Hellow")
 }
