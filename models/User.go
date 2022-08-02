7b61e574bca86d915e6bf64002be78c5f89b3e32
Carlos <carlosokumu254@gmail.com>
2022-08-03 00:47:17 +0300

    a bit of housekeeping

--- models/User.go
+++ models/User.go
@@ -34,35 +34,15 @@ func (user *User) CheckPassword(providedPassword string) error {
 }
 
 func (user *User) SendMail() {
-	//from := "carlosokumu254@gmail.com"
+
 	password := "hulisbfeulyecjpc"
 
-	//toEmailAddress := "coderokush@gmail.com"
-	//to := []string{toEmailAddress}
-
 	host := "smtp.gmail.com"
-	//port := "587"
-	//port := os.Getenv("MAILPORT")
-	//address := host + ":" + port
-
-	// subject := "Subject: This is the subject of the mail\n"
-	// //body := "This is the body of the mail"
-	// mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	// body := "<html><body><img src= "https://i.pinimg.com/originals/aa/19/47/aa1947e08757e6a7d17724677ac850e6.jpg" alt="My image" /></body></html>"
-	// message := []byte(subject + mime + body)
-
-	// auth := smtp.PlainAuth("", from, password, host)
-
-	// err := smtp.SendMail(address, auth, from, to, message)
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", "carlosokumu254@gmail.com")
 	m.SetHeader("To", "coderokush@gmail.com")
 	m.SetHeader("Subject", "Hello!")
-	m.Embed("images/android.png")
 	m.SetBody("text/html", `<img src="https://i.pinimg.com/originals/aa/19/47/aa1947e08757e6a7d17724677ac850e6.jpg" alt="My image" />`)
 
 	d := gomail.NewPlainDialer(host, 587, "carlosokumu254@gmail.com", password)
