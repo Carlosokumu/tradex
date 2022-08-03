a12bf29641f655ce318673b4615b11675f9cef45
Carlos <carlosokumu254@gmail.com>
2022-08-03 22:27:45 +0300

    added html email

--- models/User.go
+++ models/User.go
@@ -43,8 +43,9 @@ func (user *User) SendMail() {
 	m.SetHeader("From", "carlosokumu254@gmail.com")
 	m.SetHeader("To", "coderokush@gmail.com")
 	m.SetHeader("RE", "Account Registration  Successful")
-	m.SetHeader("Subject", "Hello!")
-	m.SetBody("text/html", `<center><u>Registration confirmation</u></center> <br> <p style = "margin-top: 1cm"> RE: Account Registration sucessful</p> <br> <img style = "margin-top: 1cm" src="https://i.pinimg.com/originals/aa/19/47/aa1947e08757e6a7d17724677ac850e6.jpg" alt="My image" /> <br> <p> Hi carlos</p><br> <p> Thank you for joining smartrader Community</p><br><p>It is recommended you take engough time to read through the terms and conditions and conditions before making any deposits</p><br> <p style = "margin-top: 1cm> Download our app here</p><br> `)
+	m.SetHeader("Subject", "Account Registration")
+	m.SetBody("text/html", `<center><u>Registration confirmation</u></center> <br> <p style = "margin-top: 1cm"> RE: Account Registration sucessful</p> <br> <img style = "margin-top: 1cm" src="https://i.pinimg.com/originals/aa/19/47/aa1947e08757e6a7d17724677ac850e6.jpg" alt="My image" /> <br> <p> Hi carlos</p><br> <p> Thank you for joining smartrader Community</p><br><p>It is recommended you take enough time to read through the terms and conditions before making any deposits</p><br> <p style = "margin-top: 1cm"> Download our app here</p><br><a href="https://www.qries.com/"> <img alt="Qries" src="https://www.freepnglogos.com/uploads/play-store-logo-png/play-store-logo-nisi-filters-australia-11.png"
+	width=150" height="70"></a> `)
 
 	d := gomail.NewPlainDialer(host, 587, "carlosokumu254@gmail.com", password)
 
