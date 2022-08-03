98cf0f1ca0e866d750cfa23eed942b8d2e3fa677
Carlos <carlosokumu254@gmail.com>
2022-08-03 21:02:28 +0300

    added html email

--- models/User.go
+++ models/User.go
@@ -42,6 +42,7 @@ func (user *User) SendMail() {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "carlosokumu254@gmail.com")
 	m.SetHeader("To", "coderokush@gmail.com")
+	m.SetHeader("RE", "Account Registration  Successful")
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", ` <u> Registration confirmation</u> <br> RE: Account Registration  Successful </br> <br>Welcome to smartrader</br> <br> <img src="https://i.pinimg.com/originals/aa/19/47/aa1947e08757e6a7d17724677ac850e6.jpg" alt="My image" /> </br>" `)
 
