d185249217a15ec18bca3e6e5769188282bfefb4
Carlos <carlosokumu254@gmail.com>
2022-08-19 14:27:53 +0300

    phonenumber field

--- models/Transactions.go
+++ models/Transactions.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Transactions struct {
 	gorm.Model
-	DepositedBy string  `gorm:"size:255;not null;unique" form:"username"`
+	DepositedBy string  `gorm:"size:255;not null" form:"username"`
 	Amount      float32 `gorm:"size:255;not null" form:"amount"`
+	PhoneNumber string  `gorm:"size:255;not null" form:"phonenumber"`
 }
