ece97cd8cf148cb2632167071355df324c9a2a35
Carlos <carlosokumu254@gmail.com>
2022-08-19 08:39:25 +0300

    minor bug fix

--- models/User.go
+++ models/User.go
@@ -16,7 +16,7 @@ import (
 type User struct {
 	gorm.Model
 	//FirstName   string `form:"firstname"`
-	FirstName   string   `gorm:"size:255;not null;unique" form:"firstname"`
+	FirstName   string   `gorm:"size:255;not null" form:"firstname"`
 	LastName    string   `gorm:"size:255;not null;unique" form:"lastname"`
 	Username    string   `gorm:"size:150;not null;unique" form:"username"`
 	Email       string   `gorm:"size:100;not null;unique" form:"email"`
