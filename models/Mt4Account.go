837ffd7c45b2b5753a55ee9883ba0971589f2f67
Carlos <carlosokumu254@gmail.com>
2022-08-20 11:24:26 +0300

    margin to float32

--- models/Mt4Account.go
+++ models/Mt4Account.go
@@ -7,6 +7,6 @@ type Mt4Account struct {
 	Server     string  `json:"server,omitempty"`
 	Balance    float32 `json:"balance"`
 	Equity     float32 `json:"equity,omitempty"`
-	Margin     int     `json:"margin,omitempty"`
+	Margin     float32 `json:"margin,omitempty"`
 	FreeMargin float32 `json:"freemargin,omitempty"`
 }
