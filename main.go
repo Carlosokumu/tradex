20457a351f77c6d43218eee4e9dfa330d27c0517
Carlos <carlosokumu254@gmail.com>
2022-10-04 23:01:53 -0700

    cleaning code

--- main.go
+++ main.go
@@ -20,40 +20,6 @@ func main() {
 	database.Migrate()
 
 	
-
-	//Create a client
-	//client := &http.Client{}s
-
-	// req, err := http.NewRequest("GET", "https://api.spotware.com/connect/tradingaccounts?access_token=Ct2rFyZKl7-tSXWgkXJxrScJYdMTR-sdrVc9AGDoTzw", nil)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// //Set  headers to the requests
-	// req.Header.Add("Accept", "application/json")
-	// req.Header.Add("Content-Type", "application/json")
-
-	// //Use the client to make the requests with the given [configurations]
-	// resp, err := client.Do(req)
-
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-	// }
-
-	// defer resp.Body.Close()
-
-	// bodyBytes, err := ioutil.ReadAll(resp.Body)
-
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-	// }
-
-	// var responseObject models.Response
-
-	// json.Unmarshal(bodyBytes, &responseObject)
-	// fmt.Println(responseObject.Data[0].AccountID)
-	// fmt.Println(responseObject.AccountNumber)
 	router := initRouter()
 	port := os.Getenv("PORT")
 	router.Run(":" + port)
