93e9f155b465b051512603a91839a3a2fd75ffb9
Carlos <carlosokumu254@gmail.com>
2023-06-25 15:28:06 +0300

    change Field Name

--- models/User.go
+++ models/User.go
@@ -18,20 +18,20 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName              string     `gorm:"size:255;not null" form:"firstname"`
-	LastName               string     `gorm:"size:255;not null" form:"lastname"`
-	Username               string     `gorm:"size:150;not null;unique" form:"username"`
-	Email                  string     `gorm:"size:100;not null;unique" form:"email"`
-	Password               string     `gorm:"size:100;not null" form:"password"`
-	PhoneNumber            string     `gorm:"size:50;default:null;unique" form:"phonenumber,omitempty"`
-	Balance                *float64   `gorm:"default:0" form:"balance"`
-	PercentageContribution *float64   `gorm:"default:0" form:"contribution,omitempty"`
-	FloatingProfit         *float64   `gorm:"default:0" form:"floatingprofit,omitempty"`
-	Equity                 *float64   `gorm:"default:0" form:"equity,omitempty"`
-	Positions              []Position `gorm:"foreignkey:UserID"`
+	FirstName              string            `gorm:"size:255;not null" form:"firstname"`
+	LastName               string            `gorm:"size:255;not null" form:"lastname"`
+	Username               string            `gorm:"size:150;not null;unique" form:"username"`
+	Email                  string            `gorm:"size:100;not null;unique" form:"email"`
+	Password               string            `gorm:"size:100;not null" form:"password"`
+	PhoneNumber            string            `gorm:"size:50;default:null;unique" form:"phonenumber,omitempty"`
+	Balance                *float64          `gorm:"default:0" form:"balance"`
+	PercentageContribution *float64          `gorm:"default:0" form:"contribution,omitempty"`
+	FloatingProfit         *float64          `gorm:"default:0" form:"floatingprofit,omitempty"`
+	Equity                 *float64          `gorm:"default:0" form:"equity,omitempty"`
+	Positions              []RunningPosition `gorm:"foreignkey:UserID"`
 }
 
-type Position struct {
+type RunningPosition struct {
 	gorm.Model
 	UserID     uint
 	PositionId string
