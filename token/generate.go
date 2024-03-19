b30bb9bb5d6c604ded1c5f80806df7f41742be85
Carlos <carlosokumu254@gmail.com>
2024-03-19 20:26:37 +0300

    add tokens

--- token/generate.go
+++ token/generate.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/carlosokumu/dubbedapi/models"
 	"github.com/golang-jwt/jwt"
 )
 
 // global variable
-var Secretkey = "Swingwizards"
+var Secretkey = []byte("W7-67E0Zoi5V5RY2DYP6v1DM8Lp9JshVnw_B20EM7YI=")
+
+const TOKEN_TTL = 1800
 
 // generate JWT token
 func GenerateJWT(email, username string) (string, error) {
@@ -27,4 +30,15 @@ func GenerateJWT(email, username string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func GenerateJWTWithUserModel(user models.UserModel) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   user.ID,
+		"role": user.RoleID,
+		"iat":  time.Now().Unix(),
+		"eat":  time.Now().Add(time.Second * time.Duration(TOKEN_TTL)).Unix(),
+	})
+	return token.SignedString(Secretkey)
 }
