1821d0fc828b368e61a5e4b05ad5c3d23f6593c5
Carlos <carlosokumu254@gmail.com>
2024-03-19 22:33:34 +0300

    implement verify trader

--- controllers/AdminController.go
+++ controllers/AdminController.go
@@ -1,25 +1,20 @@
 package controllers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/carlosokumu/dubbedapi/database"
 	"github.com/carlosokumu/dubbedapi/models"
+	"github.com/carlosokumu/dubbedapi/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func VerifyTrader(context *gin.Context) {
-
-	id, _ := strconv.Atoi(context.Param("id"))
-
-	fmt.Println("Id:", id)
-
-	if result := database.Instance.Table("user_models").Model(&models.UserModel{}).Where("user_name = ?", "carlos").Update("role_id", 2); result.Error != nil {
+	username := context.Query("username")
+	//TODO : Add check for username validity
+	if result := database.Instance.Table("user_models").Model(&models.UserModel{}).Where("user_name = ?", username).Update("role_id", utils.TRADER); result.Error != nil {
 		log.Fatal(result.Error)
-		fmt.Println("Cannot find User")
 	}
 
 	context.JSON(http.StatusOK, gin.H{"response": "Congratulations.You have been verified as a trader"})
